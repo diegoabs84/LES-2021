4bca65e4d759eec19ba0bbd31f6312605d98e7fc
Victor.Araujo <victor_emanuel9@hotmail.com>
2021-03-01 14:24:43 -0300

    Organizacao de arquivos e template pagina inicial

---
+++ controllers/cadastro.php
@@ -0,0 +1,44 @@
+<?php
+                                                                    // Funçoes para o cadastro e conexao com o banco de dados
+
+$login = $_POST['login'];
+$senha = MD5($_POST['senha']);
+$connect = mysql_connect('localhost:3388','root','');
+$db = mysql_select_db('Hospital_db');
+$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+$select = mysql_query($query_select,$connect);
+$array = mysql_fetch_array($select);
+$logarray = $array['login'];
+
+                                                // O Javascript dentro dos 'if-else' são pros pop-ups de avisos
+
+  if($login == "" || $login == null){
+    echo"<script language='javascript' type='text/javascript'>
+    alert('O campo login deve ser preenchido');window.location.href='
+    cadastro.html';</script>";
+
+    }else{
+      if($logarray == $login){
+
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Esse login já existe');window.location.href='
+        cadastro.html';</script>";
+        die();
+
+      }else{
+        $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
+        $insert = mysql_query($query,$connect);
+
+        if($insert){
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Usuário cadastrado com sucesso!');window.location.
+          href='login.html'</script>";
+        }else{
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Não foi possível cadastrar esse usuário');window.location
+          .href='cadastro.html'</script>";
+        }
+      }
+    }
+
+?>
