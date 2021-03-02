8738a0f4a57fb5d371cbcdc6ff129dc3b4db58e1
Tainá <tainamenezescris@gmail.com>
2021-03-02 16:02:37 -0300

    Início cadastro médico, login médico

--- controllers/cadastro_medico.php
+++ controllers/cadastro_medico.php
@@ -1,20 +1,17 @@
 <?php
                                                                     // Funçoes para o cadastro e conexao com o banco de dados
-
-$login = $_POST['login'];
+$nome = $_POST['nome'];
+$crm = $_POST['crm'];
 $senha = MD5($_POST['senha']);
-$cpf = $_POST['cpf'];
-$data = $_POST['data'];
 $connect = mysql_connect('localhost:3388','root','');
 $db = mysql_select_db('Hospital_db');
-$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+$query_select = "SELECT login FROM Medicos WHERE crm = '$crm'";
 $select = mysql_query($query_select,$connect);
 $array = mysql_fetch_array($select);
 $logarray = $array['login'];
 
-                                        // O Javascript dentro dos 'if-else' são pros pop-ups de avisos
-
-  if($login == "" || $login == null){
+                                                                    // O Javascript dentro dos 'if-else' são pros pop-ups de avisos
+if($login == "" || $login == null){
     echo"<script language='javascript' type='text/javascript'>
     alert('O campo login deve ser preenchido');window.location.href='
     cadastro.html';</script>";
@@ -24,23 +21,22 @@ $logarray = $array['login'];
 
         echo"<script language='javascript' type='text/javascript'>
         alert('Esse login já existe');window.location.href='
-        cadastro_paciente.html';</script>";
+        cadastro_medico.html';</script>";
         die();
 
       }else{
-        $query = "INSERT INTO usuarios (login,senha,cpf,data) VALUES ('$login','$senha','$cpf', '$data')";
+        $query = "INSERT INTO Medicos (nome,crm,senha) VALUES ('$nome','$crm','$senha')";
         $insert = mysql_query($query,$connect);
 
         if($insert){
           echo"<script language='javascript' type='text/javascript'>
-          alert('Usuário cadastrado com sucesso!');window.location.
-          href='login_paciente.html'</script>";
+          alert('Médico cadastrado com sucesso!');window.location.
+          href='login_medico.html'</script>";
         }else{
           echo"<script language='javascript' type='text/javascript'>
-          alert('Não foi possível cadastrar esse usuário');window.location
-          .href='cadastro_paciente.html'</script>";
+          alert('Não foi possível cadastrar esse médico');window.location
+          .href='cadastro_medico.html'</script>";
         }
       }
     }
-
 ?>
