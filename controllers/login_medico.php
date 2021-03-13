30716ab780bb73868816a2e0e97203d23b590614
Matheus Freitas <matheus.fanjos0@gmail.com>
2021-03-12 22:58:25 -0300

    Mudança do usuario devido a problema de conexao

--- controllers/login_medico.php
+++ controllers/login_medico.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "matheus";
+$password = "root";
 $dbname = "hospital";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
