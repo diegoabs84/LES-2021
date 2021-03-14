8a6959adec01c894ba694e79aa937b28795ad8a1
diegoabs84 <71224489+diegoabs84@users.noreply.github.com>
2021-03-14 13:18:31 -0300

    BE diagnostico

--- controllers/diagnostico.php
+++ controllers/diagnostico.php
@@ -17,12 +17,33 @@ function retorna($cpf_paciente, $conn){
         $row_paciente = mysqli_fetch_assoc($resultado_paciente);
         $valores['data_prevista'] = $row_paciente['data_prevista'];
         $valores['crm_solicitador'] = $row_paciente['crm_solicitador'];
+        $valores['exame'] = $row_paciente['nome_exame'];
     }else{
         $valores['data_prevista'] = 'Data não encontrada';
     }
     return json_encode($valores);
 }
 
+
 if(isset($_GET['cpf_paciente'])){
-    echo retorna($_GET['cpf_paciente'], $conn);}
+    echo retorna($_GET['cpf_paciente'], $conn);
+}
+
+if(isset($_POST['cpf_paciente'])) {
+
+    $cpf = $_POST['cpf_paciente'];
+    $data = $_POST['data_prevista'];
+    $crm = $_POST['crm_solicitador'];
+    $exame = $_POST['exame'];
+    $diagnostico = $_POST['diagnostico'];
+
+    $sql = "INSERT INTO diagnostico (cpf_paciente, crm_responsavel,exame,data_exame,diagnostico) VALUES ('$cpf', '$crm', '$exame','$data','$diagnostico')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+
 ?>
