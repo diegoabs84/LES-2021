2ecd7662dada7b365b9e598246659f36fe2079a2
Victor Araujo <victor_emanuel9@hotmail.com>
2021-04-03 16:09:43 -0300

    implementando a tela de atualizacao de cadastro em javascript

--- src/js/tela-inicial-paciente/modulos/dadosCadastrais.js
+++ src/js/tela-inicial-paciente/modulos/dadosCadastrais.js
@@ -1,7 +1,24 @@
 
 const createDadosCadastrais = () => {
+    //limpa o conteudo na div
     let conteudo = document.getElementById('conteudo-gerado');
     conteudo.innerHTML = '';
+
+
+    //container pai de atualizar paciente
+    let container = document.createElement('div');
+    container.className = 'container-atualizar-cadastro';
+
+    //adiciona o container pai dentro do elemento base
+    conteudo.appendChild(container);
+
+    //muda a variavel container para pegar o conteudo da div pai
+    container = document.querySelector('container-atualizar-cadastro');
+
+    
+
+
+
 }
 
 export default createDadosCadastrais;
