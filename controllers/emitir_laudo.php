6faeb43b7709b6181c0faa77941b1945e9ab80e8
Matheus Freitas <matheus.fanjos0@gmail.com>
2021-04-15 12:32:24 -0300

    Atualizaçao de Links

--- controllers/emitir_laudo.php
+++ controllers/emitir_laudo.php
@@ -15,7 +15,7 @@ function retorna($cpf_paciente, $conn){
     $resultado_paciente = mysqli_query($conn, $result_paciente);
     if($resultado_paciente->num_rows){
         $row_paciente = mysqli_fetch_assoc($resultado_paciente);
-        $valores['cpf_paciente'] = $row_paciente['cpf_paciente']
+        $valores['cpf_paciente'] = $row_paciente['cpf_paciente'];
         $valores['crm_responsavel'] = $row_paciente['crm_responsavel'];
         $valores['data_exame'] = $row_paciente['data_exame'];
         $valores['diagnostico'] = $row_paciente['diagnostico'];
@@ -30,13 +30,13 @@ function inserirLaudo(){
 
     $cpf = $row_paciente['cpf_paciente'];
     $laudo = $_POST['laudo'];
-    $crm_residente = $_POST['crm_residente']
+    $crm_residente = $_POST['crm_residente'];
 
     $sqlLaudo = "INSERT INTO Laudos (cpf_paciente, laudo, crm_residente) VALUES ('$cpf', '$laudo', '$crm_residente')";
 	$cadastroLaudo = $conn->query($sqlLaudo);
 
     if($cadastroLaudo){
-        echo "Cadastro de laudo realizado com sucesso!"
+        echo "Cadastro de laudo realizado com sucesso!";
     }
 
 }
