c0ada7440a2de9b13d354b05afe4700aeadfc380
Victor Emmanuel <victor_emanuel9@hotmail.com>
2021-04-27 16:28:38 -0300

    adaptacao do codigo e adicionado assets com arquivos necessarios para os icones.

--- src/js/tela-inicial-medico/main.js
+++ src/js/tela-inicial-medico/main.js
@@ -1,28 +1,45 @@
 import SolicitarExame from './modulos/solicitar_exame.js';
 import createDashboard from './modulos/dashboard.js';
-import createGeral from './modulos/geral.js';
 import createDadosCadastrais from './modulos/dadosCadastrais.js';
 
 
 window.addEventListener('load', () => {
 
 //Pegando os elementos
-let geral = document.getElementById('geral');
+let logout = document.getElementById('logout');
 let dashboard = document.getElementById('dashboard');
 let solicitarExame = document.getElementById('solicitar-exame');
 let dadosCadastrais = document.getElementById('dados-cadastrais');
 
 //Monta a pagina geral assim que a pagina carrega
-createGeral();
+
 
 //Eventos
-geral.addEventListener('click', createGeral);
 
 dashboard.addEventListener('click', createDashboard);
 
 solicitarExame.addEventListener('click', SolicitarExame);
 
 dadosCadastrais.addEventListener('click', createDadosCadastrais);
+
+logout.addEventListener('click', ()=>{});
+
+
+
+// sidebar 
+let isToggled = document.querySelector('.toggle');
+let sidebar = document.querySelector('.sidebar');
+let main = document.querySelector('.main');
+
+isToggled.addEventListener('click', toggleMenu);
+
+
+function toggleMenu(){
+    isToggled.classList.toggle('active');
+    sidebar.classList.toggle('active');
+    main.classList.toggle('active');
+}
+
 
 });
 
